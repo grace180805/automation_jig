55e5aab8b73693d0f012babb08dab20d9bf9d5f4
yong <yong.zhang2@assaabloy.com>
2024-09-19 15:40:38 +0800

    init db and create tables

--- api/src/database.py
+++ api/src/database.py
@@ -2,7 +2,7 @@ from peewee import *
 
 
 def database():
-    return SqliteDatabase('/Users/yozhan/my new/terra_forma_jig.db')
+    return SqliteDatabase('/Users/yozhan/my new/new_jig.db')
 
 
 db = database()
@@ -13,9 +13,27 @@ class BaseModel(Model):
         database = db
 
 
+class Jig(BaseModel):
+    id = PrimaryKeyField()
+    jig_id = CharField(unique=True)
+    model = CharField()
+    lock_state = IntegerField(null=0)
+    door_state = IntegerField(null=0)
+    last_topic = CharField()
+    MAC_address = CharField()
+    device_type = CharField(default="lock")
+
+
+class DeviceSupportTopic(BaseModel):
+    id = PrimaryKeyField()
+    model = CharField()
+    device_type = CharField()
+    support_topic = CharField()
+
+
 class LockAndDoorAngle(BaseModel):
     id = PrimaryKeyField()
-    device_type = CharField()
+    model = CharField()
     door_closed_angle = IntegerField(null=0)
     door_ajar_angle = IntegerField(null=0)
     door_open_angle = IntegerField(null=0)
@@ -27,45 +45,49 @@ class LockAndDoorAngle(BaseModel):
     lock_fully_unlock_angle = IntegerField(null=0)
 
 
-class LockJig(BaseModel):
-    id = PrimaryKeyField()
-    jig_id = CharField(unique=True)
-    lock_type = CharField()
-    lock_state = IntegerField(null=0)
-    door_state = IntegerField(null=0)
-    last_topic = CharField()
-    MAC_address = CharField()
-    device_type = CharField(default="lock")
-
-
-class DeviceSupportTopic(BaseModel):
-    id = PrimaryKeyField()
-    lock_id = CharField(default="default", unique=True)
-    angle = IntegerField(null=45)
-    hold_time_of_register = FloatField(null=3.0)
-    hold_time_of_operation = FloatField(null=2.0)
-
-
 def initialize_database():
-    db = database()
     db.connect()
-    db.create_tables([LockJig,
-                      ], safe=True)
+    db.create_tables([LockAndDoorAngle, Jig, DeviceSupportTopic], safe=True)
 
 
-def add_jig():
-    lock = LockJig(jig_id='jig01',
-                   lock_type='scan01', lock_state=0,
-                   door_state=0, last_topic='lock/close',
-                   MAC_address='01:as:2w',
-                   device_type='lock')
-    lock.save()
+def add_jig(jig_id, model, MAC_address):
+    jig = Jig(jig_id=jig_id,
+              model=model,
+              lock_state=2,
+              door_state=2,
+              last_topic='lock/close',
+              MAC_address=MAC_address,
+              device_type='lock')
+    jig.save()
+
+
+def add_support_topic(model, support_topic):
+    support_topic = DeviceSupportTopic(model=model,
+                                       device_type='lock',
+                                       support_topic=support_topic)
+    support_topic.save()
+
+
+def add_angle(model, door_closed_angle, door_ajar_angle, door_open_angle, lock_fully_lock_angle
+              , lock_lock_angle, lock_just_lock_angle, lock_just_unlock_angle, lock_unlock_angle
+              , lock_fully_unlock_angle):
+    angle = Jig(model=model,
+                door_closed_angle=door_closed_angle,
+                door_ajar_angle=door_ajar_angle,
+                door_open_angle=door_open_angle,
+                lock_fully_lock_angle=lock_fully_lock_angle,
+                lock_lock_angle=lock_lock_angle,
+                lock_just_lock_angle=lock_just_lock_angle,
+                lock_just_unlock_angle=lock_just_unlock_angle,
+                lock_unlock_angle=lock_unlock_angle,
+                lock_fully_unlock_angle=lock_fully_unlock_angle)
+    angle.save()
 
 
 if __name__ == '__main__':
     initialize_database()
-    # add_jig()
-    device_list = []
-    # for dev in LockJig.select():
-    #     device_list.append(dev.jig_id)
-    print(LockJig.select())
+    # add_support_topic('terra_scan01', 'lock/open')
+
+    # 遍历并打印每条记录
+    for support_topic in DeviceSupportTopic.select():
+        print(support_topic.model, support_topic.support_topic)
