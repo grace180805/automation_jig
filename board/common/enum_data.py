e57bd8885c5dbb99b47c22bdfaf171046f5776f3
amy.sheng <amy.sheng@assaabloy.com>
2024-12-02 13:57:17 +0800

    Overwrite sub_cb() function, add new function check_sum(), steps_to_two_byte_hex(), get_instructions() and clear_data()

--- board/common/enum_data.py
+++ board/common/enum_data.py
@@ -14,15 +14,15 @@ class CalibrationEnum:
     DOOR_CLOSE = 'door/close'
     DOOR_AJAR = 'door/ajar'
     FORCE_CALIBRATION = 'lock/forceCalibration'
-    
-    cmd = {
-        LOCK_OPEN :'FF FF 01 0A 03 29 32 E7 0D 00 00 D0 07 CB', # unlock  3559
-        LOCK_CLOSE : 'FF FF 01 0A 03 29 32 8F 12 00 00 D0 07 1E', # lock   4751
-        LOCK_JUST_OPEN : 'FF FF 01 0A 03 29 32 B0 0D 00 00 D0 07 02', # just unlock  3504
-        LOCK_JUST_CLOSE : 'FF FF 01 0A 03 29 32 50 12 00 00 D0 07 5D', # just lock 4688
-        LOCK_FULLY_OPEN : 'FF FF 01 0A 03 29 32 00 0D 00 00 D0 07 B2', # fylly unlock 3328
-        LOCK_FULLY_CLOSE : 'FF FF 01 0A 03 29 32 99 14 00 00 D0 07 12' # fully lock 5723
-        }
+#     
+#     cmd = {
+#         LOCK_OPEN :'FF FF 01 0A 03 29 32 E7 0D 00 00 D0 07 CB', # unlock  3559
+#         LOCK_CLOSE : 'FF FF 01 0A 03 29 32 8F 12 00 00 D0 07 1E', # lock   4751
+#         LOCK_JUST_OPEN : 'FF FF 01 0A 03 29 32 B0 0D 00 00 D0 07 02', # just unlock  3504
+#         LOCK_JUST_CLOSE : 'FF FF 01 0A 03 29 32 50 12 00 00 D0 07 5D', # just lock 4688
+#         LOCK_FULLY_OPEN : 'FF FF 01 0A 03 29 32 00 0D 00 00 D0 07 B2', # fylly unlock 3328
+#         LOCK_FULLY_CLOSE : 'FF FF 01 0A 03 29 32 99 14 00 00 D0 07 12' # fully lock 5723
+#         }
     
 class MessageEnum:
     move = b'flag=2' #
--- board/common/my_uart.py
+++ board/common/my_uart.py
@@ -28,5 +28,57 @@ class MyUART:
         else:
             return False
         
+    def check_sum(self, hex_str):
+        """
+        Check Sum = ~ (ID + Length + Instruction + Parameter1 + ... Parameter N) 若
+        括号内的计算和超出 255, 则取最低的一个字节，“~”表示取反。
+        """
+        sum_result = 0
+        # hex_num = bytes.fromhex(hex_str)
+        for i in hex_str:
+            sum_result += int(i, 16)
 
+        if sum_result > 255:
+            hex_result = hex(sum_result).upper()[-2:]  # 若括号内的计算和超出 255, 则取最低的一个字节
+        else:
+            hex_result = hex(sum_result).upper()[2:]
+
+        inverted_result = ~int(hex_result, 16) & 0xFF # 转换成十进制之后取反
+        hex_string = hex(abs(inverted_result)).upper()[2:]  # 转换成十六进制去掉结果前面的'0X'
         
+        if len(hex_string) % 2 != 0:
+            return '0' + hex_string
+        else:
+            return hex_string
+
+    def steps_to_two_byte_hex(self, steps):
+        """
+        change steps to a two-byte hexadecimal number
+        """
+        hex_string = hex(int(steps))[2:]  # 转换为十六进制字符串，去掉前面的'0x'
+        if len(hex_string) % 2 != 0:
+            hex_string = '0' + hex_string  # 确保字符串长度为偶数，方便分组
+        byte_list = [hex_string[i:i+2] for i in range(0, len(hex_string), 2)]
+        return byte_list
+
+    def get_instructions(self, steps):
+        byte_list = self.steps_to_two_byte_hex(steps)
+        list_instruction = ["01", "09", "03", "2A"]
+        list1 = ["00", "00", "E8", "03"]
+        list_instruction.append(byte_list[1].upper())
+        list_instruction.append(byte_list[0].upper())
+        list_instruction.extend(list1)
+
+        check_sum_num = self.check_sum(list_instruction)
+        list_instruction.append(check_sum_num),
+        list_instruction.insert(0, 'FF')
+        list_instruction.insert(1, 'FF')
+        my_instruction = ' '.join(list_instruction)
+        
+        return my_instruction
+
+    def clear_data(self):
+        data = 'FF FF 01 02 0A F2'
+        print('clear data: %s ' % (data))
+        hex_data = bytes.fromhex('FF FF 01 02 0A F2')  # clear data
+        self.uart.write(hex_data)
